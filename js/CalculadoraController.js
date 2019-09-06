694f1c055ad33ba569483cf8c0c402d65fefc07f
Kain989 <54442049+Kain989@users.noreply.github.com>
2019-09-06 20:46:26 -0300

    Controlador JavaScript para la Calculadora

---
+++ js/CalculadoraController.js
@@ -0,0 +1,22 @@
+const btnCero = document.getElementById("btnCero");
+const btnOne = document.getElementById("btnOne");
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
+const btnFour = document.getElementById("btnFour");
+const btnFive = document.getElementById("btnFive");
+const btnSix = document.getElementById("btnSix");
+const btnEight = document.getElementById("btnEight");
+const btnNine = document.getElementById("btnNine");
+const btnMC = document.getElementById("btnMC");
+const btnMPlus = document.getElementById("btnMPlus");
+const btnMLess = document.getElementById("btnMLess");
+const btnPlus = document.getElementById("btnPlus");
+const btnClear = document.getElementById("btnClear");
+const btnPoint = document.getElementById("btnPoint");
+const btnEqual = document.getElementById("btnEqual");
+const btnDiv = document.getElementById("btnDiv");
+const display = document.getElementById("display");
+
+btnCero.onclick(function(){
+    display.innerHTML(btnCero)
+});
